6ba2e5230a5168a43977ea3ff184bdce939232b3
Natalia Braz <naabraz@gmail.com>
2019-03-31 17:03:12 -0300

    adding button with action

--- linger-ios/ViewController.swift
+++ linger-ios/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
-
